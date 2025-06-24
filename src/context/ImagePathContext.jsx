06eacc24834af262e4306b4114154005cb965db2
Krishnendu Dutta <krishnendu.rantu@gmail.com>
2025-06-25 01:58:59 +0530

    5th commit

--- src/context/ImagePathContext.jsx
+++ src/context/ImagePathContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react';
 
 // The value should be the full path to the images folder, including the base path and /images
-export const ImagePathContext = createContext('/favina-cosmetics/images');
+export const ImagePathContext = createContext('/images');
 
 export const useImageBasePath = () => useContext(ImagePathContext);
